ded282b302f8c957c9e90672022dd287bdec6d5a
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2023-12-20 23:49:31 +0300

    lesson 21 is loading, Dialogs page (Inbox) was extended

--- src/layout/section/inbox/Inbox.tsx
+++ src/layout/section/inbox/Inbox.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import {Styles} from "./Inbox_Styles";
+import {Container} from "../../../components/Container_Styles";
 
-type InboxPropsType = {
-
-}
+type InboxPropsType = {}
 export const Inbox = (props: InboxPropsType) => {
   return (
     <Styles.Inbox>
-      Dialogs and messages are loading...
+      <div>
+        <Styles.Dialog>Dmitry</Styles.Dialog>
+        <Styles.Dialog>Ekaterina</Styles.Dialog>
+        <Styles.Dialog>Maria</Styles.Dialog>
+        <Styles.Dialog>Olga</Styles.Dialog>
+        <Styles.Dialog>Maxim</Styles.Dialog>
+      </div>
+      <div>
+        <Styles.Message>Hello, my friend!</Styles.Message>
+        <Styles.Message>How are you doing?</Styles.Message>
+        <Styles.Message>Would you like to go to the cinema?</Styles.Message>
+      </div>
     </Styles.Inbox>
   )
 }
--- src/layout/section/inbox/Inbox_Styles.ts
+++ src/layout/section/inbox/Inbox_Styles.ts
@@ -2,8 +2,21 @@ import styled from "styled-components";
 
 const Inbox = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: row;
+`
+
+const Dialog = styled.div`
+  min-width: 200px;
+  margin: 0 0 10px 0;
+`
+
+const Message = styled.div`
+  margin: 0 0 10px 0;
 `
 
 export const Styles = {
   Inbox,
+  Dialog,
+  Message,
 }
