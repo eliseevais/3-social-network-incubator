f0d782ad8bcb25c670ba8997363c5b341be4cb6c
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2024-01-06 16:57:42 +0300

    lesson_25 is loading: method map for messages, posts, dialogs

--- src/layout/section/inbox/Inbox.tsx
+++ src/layout/section/inbox/Inbox.tsx
@@ -4,48 +4,43 @@ import {DialogItem, DialogItemPropsType} from "./dialogItem/DialogItem";
 import {MessageItem} from "./messageItem/MessageItem";
 
 
-type DialogsDataPropsType = {
-  [index: number]: DialogItemPropsType
-};
+type DialogsDataPropsType = Array<DialogItemPropsType>;
 type MessagePropsType = {
   id: number;
   message: string
 };
-type MessageData = {
-  [index: number]: MessagePropsType
-}
+type MessageData = Array<MessagePropsType>;
 type InboxPropsType = {};
 
 export const Inbox = (props: InboxPropsType) => {
-  const DialogsData: DialogsDataPropsType = [
+  const dialogsData: DialogsDataPropsType = [
     {id: 1, name: 'Dmitry'},
     {id: 2, name: 'Ekaterina'},
     {id: 3, name: 'Maria'},
     {id: 4, name: 'Olga'},
     {id: 5, name: 'Maxim'}
-  ]
+  ];
 
-  const MessageData: MessageData = [
+  let dialogsElements = dialogsData
+    .map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>);
+
+  const messageData: MessageData = [
     {id: 1, message: 'Hello, my friend!'},
     {id: 2, message: 'How are you doing?'},
     {id: 3, message: 'Would you like to go to the cinema?'},
     {id: 4, message: 'No, thanks, I will stay at home to study.'},
-  ]
+  ];
+
+  let messagesElements = messageData
+    .map(message => <MessageItem message={message.message}/>);
+
   return (
     <Styles.Inbox>
       <div>
-        <DialogItem name={DialogsData[0].name} id={DialogsData[0].id}/>
-        <DialogItem name={DialogsData[1].name} id={DialogsData[1].id}/>
-        <DialogItem name={DialogsData[2].name} id={DialogsData[2].id}/>
-        <DialogItem name={DialogsData[3].name} id={DialogsData[3].id}/>
-        <DialogItem name={DialogsData[4].name} id={DialogsData[4].id}/>
-
+        {dialogsElements}
       </div>
       <div>
-        <MessageItem message={MessageData[0].message}/>
-        <MessageItem message={MessageData[1].message}/>
-        <MessageItem message={MessageData[2].message}/>
-        <MessageItem message={MessageData[3].message}/>
+        {messagesElements}
       </div>
     </Styles.Inbox>
   )
--- src/layout/section/main/myPosts/MyPosts.tsx
+++ src/layout/section/main/myPosts/MyPosts.tsx
@@ -8,19 +8,19 @@ type PostPropsType = {
   message: string;
   likesCount: number
 };
-type PostDataPropsType = {
-  [index: number]: PostPropsType
-};
+type PostDataPropsType = Array<PostPropsType>;
 
 export const MyPosts = () => {
   const postsData: PostDataPropsType = [
-    {id: 1, message: 'It\'s my first post.', likesCount: 9 },
-    {id: 2, message: 'Hello! How are you doing?', likesCount: 15 },
-    {id: 3, message: 'Winter is coming to our city', likesCount: 3 },
-    {id: 4, message: 'Happy New Year!', likesCount: 7 },
-    {id: 5, message: 'The weather was -24 today.', likesCount: 10 },
-    {id: 6, message: 'I like it-incubator!', likesCount: 23 }
-  ]
+    {id: 1, message: 'It\'s my first post.', likesCount: 9},
+    {id: 2, message: 'Hello! How are you doing?', likesCount: 15},
+    {id: 3, message: 'Winter is coming to our city', likesCount: 3},
+    {id: 4, message: 'Happy New Year!', likesCount: 7},
+    {id: 5, message: 'The weather was -24 today.', likesCount: 10},
+    {id: 6, message: 'I like it-incubator!', likesCount: 23}
+  ];
+  let postsElements = postsData
+    .map(post => <Post message={post.message} likesCount={post.likesCount}/>)
   return (
     <div>
       <ProfileInfo/>
@@ -28,12 +28,7 @@ export const MyPosts = () => {
         <Styles.NewPostTextarea/>
         <Styles.AddPostButton>Add post</Styles.AddPostButton>
       </Styles.NewPostWrapper>
-      <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-      <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
-      <Post message={postsData[2].message} likesCount={postsData[2].likesCount}/>
-      <Post message={postsData[3].message} likesCount={postsData[3].likesCount}/>
-      <Post message={postsData[4].message} likesCount={postsData[4].likesCount}/>
-      <Post message={postsData[5].message} likesCount={postsData[5].likesCount}/>
+      {postsElements}
     </div>
   );
 };
